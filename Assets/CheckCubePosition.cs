d3a17d3e3b0681a63cdc2fdd5421601ba3b79d2b
huyhoangsec <hoangdangxa@gmail.com>
2026-01-17 22:53:04 +0700

    Baitap

---
+++ Assets/CheckCubePosition.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CheckCubePosition : MonoBehaviour
+{
+    void Start()
+    {
+        // World Space position
+        Vector3 worldPos = transform.position;
+
+        // Local Space position (so với Parent)
+        Vector3 localPos = transform.localPosition;
+
+        Debug.Log("World Position of Cube: " + worldPos);
+        Debug.Log("Local Position of Cube: " + localPos);
+    }
+}
